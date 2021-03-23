511260377021c8d1e4c269c0e8e0cb459a939fea
tinkericlee <eric.xiangfu.lee@gmail.com>
2021-03-23 12:49:20 +0800

    Fix spark build function arguments error

--- spark/docker-build.sh
+++ spark/docker-build.sh
@@ -1,49 +1,49 @@
 WORK_DIR=$(cd "`dirname "$0"`"; pwd)
 
-function download_and_extract_spark_dist(spark_version, hadoop_version, download_base_path) {
+function download_and_extract_spark_dist() {
     # First download built spark distribution package from apache
     # referrence: https://spark.apache.org/downloads.html
-    wget https://downloads.apache.org/spark/spark-$spark_version/spark-$spark_version-bin-hadoop$hadoop_version.tgz -O $download_base_path/spark-$spark_version-bin-hadoop$hadoop_version.tgz
-    tar -xf $download_base_path/spark-$spark_version-bin-hadoop$hadoop_version.tgz -C $download_base_path
+    wget https://downloads.apache.org/spark/spark-$1/spark-$1-bin-hadoop$2.tgz -O $3/spark-$1-bin-hadoop$2.tgz
+    tar -xf $3/spark-$1-bin-hadoop$2.tgz -C $3
 }
 
-function build_and_push_spark(spark_version, hadoop_version, scala_version, spark_home) {
+function build_and_push_spark() {
     # build spark
-    /bin/bash $spark_home/bin/docker-image-tool.sh -r $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version -t alpha build
+    /bin/bash $4/bin/docker-image-tool.sh -r $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3 -t alpha build
     # fixed tag name pattern
-    docker tag $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version/spark:alpha $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version:alpha 
+    docker tag $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3/spark:alpha $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3:alpha 
     # already logged in Makefile
-    docker push $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version:alpha 
+    docker push $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3:alpha 
 }
 
 # build spark-with-r
-function build_and_push_spark_r(spark_version, hadoop_version, scala_version, spark_home) {
+function build_and_push_spark_r() {
     # build spark
-    /bin/bash $spark_home/bin/docker-image-tool.sh -r $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version -t alpha -R $spark_home/kubernetes/dockerfiles/spark/bindings/R/Dockerfile build
+    /bin/bash $4/bin/docker-image-tool.sh -r $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3 -t alpha -R $4/kubernetes/dockerfiles/spark/bindings/R/Dockerfile build
     # fixed tag name pattern
-    docker tag $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version/spark-r:alpha $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version:alpha-with-r
+    docker tag $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3/spark-r:alpha $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3:alpha-with-r
     # already logged in Makefile
     docker push $DOCKER_REGISTRY/spark-3.1.1-without-hadoop-with-r:alpha 
 }
 
 # build spark-with python
-function build_and_push_spark_py(spark_version, hadoop_version, scala_version, spark_home) {
+function build_and_push_spark_py() {
     # build spark
-    /bin/bash $spark_home/bin/docker-image-tool.sh -r $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version -t alpha -p $spark_home/kubernetes/dockerfiles/spark/bindings/python/Dockerfile build
+    /bin/bash $4/bin/docker-image-tool.sh -r $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3 -t alpha -p $4/kubernetes/dockerfiles/spark/bindings/python/Dockerfile build
     # fixed tag name pattern
-    docker tag $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version/spark-py:alpha $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version:alpha-with-py
+    docker tag $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3/spark-py:alpha $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3:alpha-with-py
     # already logged in Makefile
-    docker push $DOCKER_REGISTRY/spark-$spark_version-hadoop-$hadoop_version-scala-$scala_version:alpha-with-py
+    docker push $DOCKER_REGISTRY/spark-$1-hadoop-$2-scala-$3:alpha-with-py
 }
 
-function build_spark_image(spark_version, hadoop_version, scala_version, work_dir) {
-    download_and_extract_spark_dist($spark_version, $hadoop_version, $WORK_DIR)
-    build_and_push_spark($spark_version, $hadoop_version, $scala_version, "$work_dir/spark-$spark_version-bin-hadoop$hadoop_version")
-    build_and_push_spark_r($spark_version, $hadoop_version, $scala_version, "$work_dir/spark-$spark_version-bin-hadoop$hadoop_version")
-    build_and_push_spark_py($spark_version, $hadoop_version, $scala_version, "$work_dir/spark-$spark_version-bin-hadoop$hadoop_version")
+function build_spark_image() {
+    download_and_extract_spark_dist $1 $2 $4
+    build_and_push_spark $1 $2 $3 "$4/spark-$1-bin-hadoop$2"
+    build_and_push_spark_r $1 $2 $3 "$4/spark-$1-bin-hadoop$2"
+    build_and_push_spark_py $1 $2 $3 "$4/spark-$1-bin-hadoop$2"
 }
 
-build_spark_image('3.1.1', '3.2', '2.12', $WORK_DIR)
-build_spark_image('3.1.1', '2.7', '2.12', $WORK_DIR)
-build_spark_image('2.4.7', '2.7', '2.11', $WORK_DIR)
-build_spark_image('2.4.7', '2.6', '2.11', $WORK_DIR)
+build_spark_image '3.1.1' '3.2' '2.12' $WORK_DIR
+build_spark_image '3.1.1' '2.7' '2.12' $WORK_DIR
+build_spark_image '2.4.7' '2.7' '2.11' $WORK_DIR
+build_spark_image '2.4.7' '2.6' '2.11' $WORK_DIR
